39dc816394a8dcf68973bf6ba0b23b5552e65c1c
Mezza M <shawnm89@hotmail.co.uk>
2021-09-21 19:09:51 +0100

    updated

--- PythonApplicationnNew/PythonApplicationnNew.py
+++ PythonApplicationnNew/PythonApplicationnNew.py
@@ -191,4 +191,55 @@ n = int(input("enter a number : "))
 
 print (n > 100)
 
+#Lab 3.1.1.10
+#An input was needed then responses to the correct and incorrect asnwers.
+#If, elif and else were used. : is needs after the if, elif and else command
 
+flower = input("Input a flower name:")
+
+if flower == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+    
+elif flower == "spathiphyllum":
+    print ("No, I want a big Spathiphyllum!")
+    
+else: 
+    print ("Spathiphyllum! Not", flower + "!")
+
+#Lab 3.1.1.11
+#This section uses if,, else.
+#If income < 85528, income is x 18% and - 556.02 = tax
+#If income is > 85528, minus income with 85528, x surplus by 32%
+#Add 14839.02 = tax
+#If tax <= 0, no tax is paid.
+
+income = float(input("Enter the annual income: "))
+
+if income < 85528:
+	tax = income * 0.18 - 556.02
+else:
+    tax = (income - 85528) * 0.32 + 14839.02
+    
+if tax <= 0:
+    tax = 0
+  
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
+
+#Lab 3.1.1.12
+#If year is less than 1582 print("not") text
+#If year is not divisable by 4 or 400 print ("common year")
+#If year is not divisable by 100 or anything else print("Leap year")
+
+year = int(input("Enter a year: "))
+
+if year < 1582:
+    print("Not within the Gregorian calendar period")
+    
+else:
+    if (year % 4) + (year % 400) != 0:
+        print("Common year")
+    elif year % 100 != 0:
+        print("Leap year")
+    else:
+        print("Leap year")
--- PythonApplicationnNew/myworking.py
+++ PythonApplicationnNew/myworking.py
@@ -42,6 +42,7 @@ a = int(input("enter number : "))
 
 #How to use the if else code
 
+# long version
 number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
 
@@ -53,3 +54,95 @@ else:
 
 # Print the result
 print("The larger number is:", larger_number)
+
+# short version
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2: larger_number = number1
+else: larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
+# If more than two numbers
+
+# Read three numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# We temporarily assume that the first number
+# is the largest one.
+# We will verify this soon.
+largest_number = number1
+
+# We check if the second number is larger than current largest_number
+# and update largest_number if needed.
+if number2 > largest_number:
+    largest_number = number2
+
+# We check if the third number is larger than current largest_number
+# and update largest_number if needed.
+if number3 > largest_number:
+    largest_number = number3
+
+# Print the result
+print("The largest number is:", largest_number)
+
+# Using the max/min function
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# Check which one of the numbers is the greatest
+# and pass it to the largest_number variable.
+
+largest_number = max(number1, number2, number3)
+
+# Print the result.
+print("The largest number is:", largest_number)
+
+# if, elif and else using words
+
+flower = input("Input a flower name:")
+
+if flower == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+    
+elif flower == "spathiphyllum":
+    print ("No, I want a big Spathiphyllum!")
+    
+else: 
+    print ("Spathiphyllum! Not", flower + "!")
+
+    #Workin out tax or number values
+    income = float(input("Enter the annual income: "))
+
+if income < 85528:
+	tax = income * 0.18 - 556.02
+else:
+    tax = (income - 85528) * 0.32 + 14839.02
+    
+if tax <= 0:
+    tax = 0
+  
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
+
+#Working out how to create code for leap years. 
+#Remember to add : and the end of the if, elif, else function.
+
+year = int(input("Enter a year: "))
+
+if year < 1582:
+    print("Not within the Gregorian calendar period")
+    
+else:
+    if (year % 4) + (year % 400) != 0:
+        print("Common year")
+    elif year % 100 != 0:
+        print("Leap year")
+    else:
+        print("Leap year")
