86da2bb2941a1ad295b316fb6e6e4f97a7de1e6f
Mezza M <shawnm89@hotmail.co.uk>
2021-09-27 17:24:50 +0100

    updated 27/09/21

---
+++ PythonApplicationnNew/module4.py
@@ -0,0 +1,101 @@
+####################### Lab 4.3.1.6
+
+def is_year_leap(year):
+    if ((year) % 2 == 0): #Is the year even.
+        return True #Returns True if it is
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+	yr = test_data[i]
+	print(yr,"->",end="")
+	result = is_year_leap(yr)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
+
+
+####################### Lab 4.3.1.7
+#I used the light bulb.
+
+def is_year_leap(year):
+	if year % 4 != 0:
+		return False
+	elif year % 100 != 0:
+		return True
+	elif year % 400 != 0:
+		return False
+	else:
+		return True
+
+def days_in_month(year,month):
+	if year < 1582 or month < 1 or month > 12:
+		return None
+	days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	res  = days[month - 1]
+	if month == 2 and is_year_leap(year):
+		res = 29
+	return res
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [ 2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+	yr = test_years[i]
+	mo = test_months[i]
+	print(yr,mo,"-> ",end="")
+	result = days_in_month(yr, mo)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
+
+####################### Lab 4.3.1.8
+#Light Bulb
+def is_year_leap(year):
+	if year % 4 != 0:
+		return False
+	elif year % 100 != 0:
+		return True
+	elif year % 400 != 0:
+		return False
+	else:
+		return True
+
+def days_in_month(year, month):
+	if year < 1582 or month < 1 or month > 12:
+		return None
+	days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	res  = days[month - 1]
+	if month == 2 and is_year_leap(year):
+		res = 29
+	return res
+
+def day_of_year(year, month, day):
+	days = 0
+	for m in range(1, month):
+		md = days_in_month(year, m)
+		if md == None:
+			return None
+		days += md
+	md = days_in_month(year, month)
+	if day >= 1 and day <= md:
+		return days + day
+	else:
+		return None
+
+print(day_of_year(2000, 12, 31))
+
+####################### Lab 4.3.1.9
+
+def is_prime(num):
+    for i in range(2, int(1 + num ** 0.5)):
+        if num % i == 0:
+            return False
+    return True
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
