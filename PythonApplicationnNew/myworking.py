161423d2ea57c668248f74a8b8ffe495b59a1301
Mezza M <shawnm89@hotmail.co.uk>
2021-09-29 08:17:22 +0100

    1

--- PythonApplicationnNew/myworking.py
+++ PythonApplicationnNew/myworking.py
@@ -253,3 +253,46 @@ for i in range(num):
 
 my_list.sort()
 print(my_list)
+
+####### working out BMI
+
+def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or \
+    weight < 20 or weight > 200:
+        return None
+
+    return weight / height ** 2
+
+
+print(bmi(100, 1.65))
+
+def lb_to_kg(lb):
+    return lb * 0.45359237
+
+
+print(lb_to_kg(184.9))
+
+def kg_to_lb(kg):
+    return kg * 2.2
+
+
+print(kg_to_lb(84))
+
+######### Finonacci numbers
+
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+
+    elem_1 = elem_2 = 1
+    the_sum = 0
+    for i in range(3, n + 1):
+        the_sum = elem_1 + elem_2
+        elem_1, elem_2 = elem_2, the_sum
+    return the_sum
+
+
+for n in range(1, 99):  # testing
+    print(n, "->", fib(n))
